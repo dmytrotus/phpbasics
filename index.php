6c0eb514bc6704850b9f0c43edf6cc89d727d305
Dmytro Tus <dmytrotus@gmail.com>
2019-08-06 20:37:26 +0200

    Some new stuff

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-echo "Hello World!";
+echo "Hello World!".PHP_EOL;
 
-?>
-<div>
-<?php
+
 
 $string = '45';
 
@@ -15,3 +13,34 @@ if(is_int($string)){
 	echo $string;
 } else { echo 'This string is not integer';}
 
+
+
+class Foo
+{
+    public function printItem($string)
+    {
+        echo 'Foo: ' . $string . PHP_EOL;
+    }
+    
+    public function printPHP()
+    {
+        echo 'PHP просто супер.' . PHP_EOL;
+    }
+}
+
+class Bar extends Foo
+{
+    public function printItem($string)
+    {
+        echo 'Bar: ' . $string . PHP_EOL;
+    }
+}
+
+$foo = new Foo();
+$bar = new Bar();
+$foo->printItem('baz'); // Выведет: 'Foo: baz'
+$foo->printPHP();       // Выведет: 'PHP просто супер' 
+$bar->printItem('baz'); // Выведет: 'Bar: baz'
+$bar->printPHP();       // Выведет: 'PHP просто супер'
+
+?>
